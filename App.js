6f105604acbb01a5013769bb190b34fcfecae534
Ingrid Silva <139189235+ingridlorrane-silva@users.noreply.github.com>
2025-08-12 22:27:06 -0300

    Update App.js

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ return(
 <View style={styles.content}>
 <Text style={{fontSize: '50', color: 'orange'}}>Aplicações!</Text>
 <Text> Seja Bem vindo {nome}. Sua idade é {idade}</Text>
-<Image style = {style.tinyLogo} source={require('./src/img/pensador.jpg')}/>
+<Image style = {styles.tinyLogo} source={require('./src/img/pensador.jpg')}/>
 </View>
 );
 
@@ -18,12 +18,10 @@ return(
 const styles = StyleSheet.create({
   content:{
     flex:1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'yellow'
-  }
-})
-
-const style = StyleSheet.create({
+  },
   tinyLogo:{
    width: 250, height: 250, backgroundColor: 'red'}
-  })
+})
+
 
 export default App;
