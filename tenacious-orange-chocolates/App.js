32efa2ba7645a8803f7964097e5749a2c5319653
Ingrid Silva <139189235+ingridlorrane-silva@users.noreply.github.com>
2025-08-12 22:02:47 -0300

    Update App.js

--- tenacious-orange-chocolates/App.js
+++ tenacious-orange-chocolates/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import{View, Text, StyleSheet} from 'react-native'
+import{View, Text, StyleSheet, Image} from 'react-native'
 
 const App = () => {
 
 return(
 <View style={styles.content}>
 <Text style={{fontSize: '50', color: 'orange'}}>Hello World!</Text>
+  <Image style = {style.tinyLogo} source{require('./src/img/pensador.png)}/>
 </View>
 );
 
@@ -15,5 +16,10 @@ const styles = StyleSheet.create({
   content:{
     flex:1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'yellow'
   }
+  
+  tinyLogo:{
+   flex:1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'yellow'
+  }
 })
+
 export default App;
